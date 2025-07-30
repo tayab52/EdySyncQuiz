206b75675cbd5ddecb86d976549ca2ec9aa474af
Ahmad Akram <87974460+AhmadAkramDev@users.noreply.github.com>
2025-07-30 19:49:47 +0500

    set response

--- OnlineQuizSystem/Controllers/Language/LanguageController.cs
+++ OnlineQuizSystem/Controllers/Language/LanguageController.cs
@@ -1,4 +1,6 @@
-﻿using CommonOperations.Methods;
+﻿using Application.DataTransferModels.ResponseModel;
+using CommonOperations.Constants;
+using CommonOperations.Methods;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationAPI.Controllers.Language
@@ -11,16 +13,22 @@ namespace PresentationAPI.Controllers.Language
         [HttpGet]
         public IActionResult GetLanguages()
         {
+            ResponseVM response = ResponseVM.Instance;
+
             var results = Methods.ExecuteStoredProceduresList("SP_GetLanguages", null!);
+            var languages = results.Result.Select(lang => (string)lang.Language).ToList();
 
             if (!results.Result.Any())
             {
-                return NotFound(new { Message = "No languages found." });
+                response.StatusCode = ResponseCode.NotFound;
+                response.ErrorMessage = "No languages found.";
+                return NotFound(response);
             }
+            response.StatusCode = ResponseCode.Success;
+            response.Data = languages;
+            response.ResponseMessage = "Success!";
 
-            var languages = results.Result.Select(lang => (string)lang.Language).ToList();
-
-            return Ok(languages);
+            return Ok(response);
         }
 
     }
