31d78e431cd2879dfa7c25820c058ee8dd9956a7
bilal-dev-texel <bilal@wordsensesoft.net>
2025-07-31 16:49:20 +0500

    fix quiz controller

--- OnlineQuizSystem/Controllers/Quiz/QuizController.cs
+++ OnlineQuizSystem/Controllers/Quiz/QuizController.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Context;
+﻿using Application.DataTransferModels.ResponseModel;
+using CommonOperations.Constants;
+using Infrastructure.Context;
 using Infrastructure.Services.Gemini;
 using Infrastructure.Services.Token;
 using Microsoft.AspNetCore.Authorization;
@@ -9,31 +11,23 @@ using System.Diagnostics;
 namespace PresentationAPI.Controllers.Quiz
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/quiz")]
     [Authorize] 
     public class QuizController(ClientDBContext clientDBContext, TokenService tokenService, GeminiQuizService quizService) : Controller
     {
-        [HttpGet("Generate-Quiz")]
+        [HttpGet("generate-quiz")]
         public async Task<IActionResult> GenerateQuiz()
         {
-            //var s = Stopwatch.StartNew();
-
-            //var sw = Stopwatch.StartNew();
+            ResponseVM response = ResponseVM.Instance;
             Guid userID = tokenService.UserID;
-            //sw.Stop();
-            //Console.WriteLine($"Token service took {sw.ElapsedMilliseconds} ms");
-            
-            //var sw2 = Stopwatch.StartNew();
             var user = clientDBContext.Users.Find(userID);
-            //sw2.Stop();
-            //Console.WriteLine($"DB Operation took {sw2.ElapsedMilliseconds} ms");
-
             if (user == null || string.IsNullOrWhiteSpace(user.Interests))
-                return BadRequest("User or interests not found");
-
+            {
+                response.StatusCode = ResponseCode.BadRequest;
+                response.ErrorMessage = "User or interests not found";
+                return BadRequest(response);
+            }
             var quizJson = await quizService.GenerateQuizFromInterestsAsync(user.Interests, (int)user.Level!);
-            //s.Stop();
-            //Console.WriteLine($"Total Time took {s.ElapsedMilliseconds} ms");
             return Ok(quizJson);
         }
     }
